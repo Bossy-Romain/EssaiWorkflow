9e6522ed0b25bf7737f31f6860f1533ee54a52b4
BossyRomain <romain.bossy@cea.fr>
2025-07-10 09:26:58 +0200

    first commit

---
+++ essai/onepackage.py
@@ -0,0 +1,9 @@
+def add(x, y):
+    """
+    Simple adding function.
+
+    :param x(int|float): first value.
+    :param y(int|float): second value.
+    :returns: the sum of x and y.
+    """
+    return x + y
